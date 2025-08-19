a2539eb7268b31591946516e809938750599dc36
Luca Milone <lucamilone96@gmail.com>
2025-08-19 19:04:43 +0200

    First Commit

---
+++ src/main/java/com/betacom/retrogames/RetroGamesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.retrogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGamesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroGamesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betacom/retrogames/RetroGamesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.retrogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroGamesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
